59af2633e4736983c202515712857a553bd6344e
AHuusom <anders@sadhu.dk>
2016-06-28 12:03:23 +0200

    Add files via upload

---
+++ ConnectAzurePS.ps1
@@ -0,0 +1,22 @@
+﻿Select-AzureSubscription -SubscriptionName "GKIT-Prod"
+$cloudservicename = "GreveK-DC"
+$name = "dc-a01"
+$vm = get-azurevm -ServiceName $cloudservicename -name $name
+$winRMCert = ($vm | select -ExpandProperty vm).DefaultWinRMCertificateThumbprint
+
+if (!$winRMCert)
+{
+    write-Host ("**ERROR**: Unable to find WinRM Certificate for virtual machine '"+$virtualMachineName)
+    $vm = Get-AzureVM -ServiceName $cloudServiceName -Name $virtualMachineName
+    if (!$vm)
+    {
+        write-Host ("virtual machine "+$virtualMachineName+" not found in cloud service "+$cloudServiceName);
+    }
+    Exit
+}
+
+$AzureX509cert = Get-AzureCertificate -ServiceName $cloudServiceName -Thumbprint $winRMCert -ThumbprintAlgorithm sha1
+
+$certTempFile = [IO.Path]::GetTempFileName()
+$AzureX509cert.Data | Out-File $certTempFile
+
